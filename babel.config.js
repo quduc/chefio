6d521f722c5160e486a89f883577a1096f92858e
= <=>
2021-07-24 17:50:44 +0700

    first commit

---
+++ babel.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          tests: ['./tests/'],
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+        },
+      },
+    ],
+  ],
+  presets: ['module:metro-react-native-babel-preset'],
+};
